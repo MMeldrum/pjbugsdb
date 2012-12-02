7730db5a0d6b52e0bfe3d0dd7896506a26d07496
Martin Meldrum <martin.meldrum@gmail.com>
2012-12-02 20:11:25 +0000

    init

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -23,17 +23,17 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
     }
-    production {
-        dataSource {
-            dbCreate = "update"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
+	production {
+	    dataSource {
+	        dbCreate = "update"
+	        driverClassName = "org.postgresql.Driver"
+	        dialect = org.hibernate.dialect.PostgreSQLDialect
 
-            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+	        uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 
-            url = "jdbc:postgresql://"+uri.host+uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
-        }
-    }
+	        url = "jdbc:postgresql://"+uri.host+uri.path
+	        username = uri.userInfo.split(":")[0]
+	        password = uri.userInfo.split(":")[1]
+	    }
+	}
 }
